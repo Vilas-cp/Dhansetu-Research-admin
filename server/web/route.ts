1d2dea370656afb295fc6d4d4f476fb8d6d6018d
ManojaD2004 <manojadkc2004@gmail.com>
2026-03-03 00:41:01 +0530

    details

--- server/web/route.ts
+++ server/web/route.ts
@@ -265,6 +265,45 @@ v1Routes.get("/buy/details/:rzpayid", async (req, res) => {
   }
 });
 
+v1Routes.get("/buy/details/:rzpayid", async (req, res) => {
+  try {
+    const rzpayId = req.params.rzpayid;
+    const contactDetails = req.body;
+    // console.log(req.body);
+    const webDb = new WebDB();
+    const orderDetails = await webDb.getSuccessOrderDetails(rzpayId);
+    if (orderDetails === null) {
+      res.status(400).send({
+        status: "fail",
+        data: {
+          message: "Database is offline, or server error!",
+        },
+      });
+      return;
+    }
+    res.status(200).send({
+      status: "success",
+      data: {
+        orderDetails,
+      },
+    });
+    return;
+  } catch (error: any) {
+    console.log(
+      chalk.red(
+        `Error: ${error?.message}, for user id ${req.signedCookies?.userName}`,
+      ),
+    );
+    res.status(400).send({
+      status: "fail",
+      error: error,
+      data: {
+        message: "Internal Server Error!",
+      },
+    });
+  }
+});
+
 
 v1Routes.post("/buy/order/rzpay/:timeId/:optId", async (req, res) => {
   try {
